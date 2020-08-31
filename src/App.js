899c8dfbaa099fa1c21d1fe3c09cd1e4f5a283e2
Artyom Bakulev <i@artbakulev.com>
2020-08-31 20:28:33 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
 import CounterList from "./components/CounterList";
+import logo from "./img/logo.gif";
+import "@vkontakte/vkui/dist/vkui.css";
+import "antd/dist/antd.css";
 
 function App() {
-    return (
-        <div className="App">
-            <CounterList counters={[
-                {number: '985 346', text: 'Выявлено заболевших'},
-                {number: '4 941', 'text': 'Выявлено заболевших за последние сутки'},
-                {number: '> 36 млн', 'text': 'Проведено тестов'}
-            ]}/>
-        </div>
-    );
+  return (
+    <div className="app">
+      <div className="logo" style={{ backgroundImage: `url(${logo})` }} />
+      <CounterList
+        counters={[
+          { number: "985 346", text: "Выявлено заболевших" },
+          { number: "4 941", text: "Выявлено заболевших за последние сутки" },
+          { number: "695", text: "Новых случаев в Москве" },
+          { number: "806 982", text: "Человека выздоровело" },
+        ]}
+      />
+    </div>
+  );
 }
 
 export default App;
